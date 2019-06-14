a4fa856e65000d938775aea4889d90820c4666b3
GwangMin <bgm537@saybgm.me>
2019-06-14 15:49:13 +0900

    redux, react-router 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import store from './core/redux';
+
+import Main from './container/Main';
+
 import './App.scss';
 
-const App = () => <div className="App" />;
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Main} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>
+);
 
 export default App;
